2b12637b2e7b8336c0e188560a69356207f121f3
ADAI <ll1322688810@gmail.com>
2020-11-26 18:02:49 +0800

    再次提交登录功能模块

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import './assets/css/global.css'
 // 导入axios
 import axios from 'axios'
 // 设置请求的根路径
-axios.defaults.baseURL =  'http://127.0.0.1:8888/api/private/v1/'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 // 将axios挂载到Vue的原型对象上，每一个Vue组件都能通过this访问到$http发ajax请求
 Vue.prototype.$http = axios
 
