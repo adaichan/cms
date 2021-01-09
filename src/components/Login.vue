b7218bc75ff523380ae7e4c16ac4b257a891c9dc
adaichan <ll1322688810@gmail.com>
2021-01-09 13:57:03 +0800

    完成了分类参数模块

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -46,8 +46,8 @@ export default {
     return {
       // 登录表单的数据绑定对象
       loginForm: {
-        username: '',
-        password: ''
+        username: 'admin',
+        password: '123456'
       },
       // 登录表单的验证规则对象
       loginFormRules: {
